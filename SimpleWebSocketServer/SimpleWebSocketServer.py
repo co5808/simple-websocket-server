4fd6bcb16efb502d63c421df24995a82b593fb8d
cgchae-Mac <cgchae@aspringcloud.com>
2020-08-25 14:33:11 +0900

    send to client message : SEc-WebSocket-Protocol : soap

--- SimpleWebSocketServer/SimpleWebSocketServer.py
+++ SimpleWebSocketServer/SimpleWebSocketServer.py
@@ -51,7 +51,7 @@ HANDSHAKE_STR = (
    "Upgrade: WebSocket\r\n"
    "Connection: Upgrade\r\n"
    "Sec-WebSocket-Accept: %(acceptstr)s\r\n\r\n"
-   "Sec-WebSocket-Protocol: chat, superchat\r\n\r\n"
+   "Sec-WebSocket-Protocol: soap\r\n\r\n"
 )
 
 FAILED_HANDSHAKE_STR = (
